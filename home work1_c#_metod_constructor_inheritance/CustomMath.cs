de8947ec2823055aa93660ae1e8edec0b13102cc
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-25 22:24:39 +0400

    Add files via upload

---
+++ home work1_c#_metod_constructor_inheritance/CustomMath.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_work1_c__metod_constructor_inheritance
+{
+    internal class CustomMath
+    {
+        //public int SumOfOdds(int[] array)
+        //{
+        //    int sum = 0;
+        //    foreach (int element in array)
+        //    {
+        //        if (element % 2 != 0)
+        //        {
+        //            sum += element;
+        //        }
+        //    }
+        //    return sum;
+        //}
+
+        //public string SingleAndDouble(int numb)
+        //{
+        //    if (numb % 2 == 0)
+        //    {
+        //        return "Cutdur";
+        //    }
+        //    else
+        //    {
+        //        return "Tekdir";
+        //    }
+        //}
+
+        //public int SquareOfOdds(int[] array)
+        //{
+        //    int sum = 0;
+        //    foreach (int elems in array)
+        //    {
+        //        if (elems % 2 == 0)
+        //        {
+        //            sum += elems;
+        //        }
+        //    }
+        //    return sum * sum;
+        //}
+
+        //public int SumOfNumb(int n)
+        //{
+        //    int count = 1;
+        //    for (int i = 1; i <= n; i++)
+        //    {
+        //        count += i;
+        //    }
+        //    return count;
+        //}
+
+      public int SumOfOdds(int[] array)
+      {
+         int sum = 0;
+
+         foreach (var elems in array)
+         {
+            if (elems % 2 != 0)
+            {
+              sum += elems;
+            }
+         }
+         return sum;
+      }
+
+      public string SingleAndDouble(int n) 
+      {
+            if (n % 2 == 0) 
+            {
+                return "cutdur";            
+            }
+            else 
+            {
+                return "tekdir";
+            }
+      }
+
+
+      public int SquareOfOdds(int[] array) 
+      {
+        int sum = 0;
+
+            foreach (var elems in array)
+            {
+                if (elems % 2 == 0) 
+                {
+                    sum += elems;
+                }
+                
+            }
+            return sum * sum;
+      }
+      
+      public int SumOfnumb(int x) 
+      {
+         int Count = 0;
+
+         for (int i = 1; i <= x; i++)
+         {
+             Count+=i;
+         }
+         return Count;
+            
+      }
+
+
+    }
+}
---
+++ home work1_c#_metod_constructor_inheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//2) CustomMath adinda class yaradin, hemin clasin icinde ashagidaki methodlar olacaq.
+//Hemin methodlari proqram classda cagirib ishledin.
+//
+// 1)Method array qebul edir, arrayin icerisindeki tek ededlerin cemini tapin.
+// 2)Methoda gelen ededin tek ve ya cut olub olmamasini gosterin.
+// 3) Methoda gelen arrayin elementlerininden cut olanlarinin ceminin kvadratini tapmaq.
+// 4) Methoda n ededi gelir, 1-den n ededine qeder olan ededlerin hasilini tapin.
+
+
+
+
+
+
+using home_work1_c__metod_constructor_inheritance;
+
+
+//CustomMath math = new CustomMath();
+
+//int[] numbers = { 1, 2, 3, 4, 5 };
+//int SumOfOdds = math.SumOfOdds(numbers);
+//Console.WriteLine($"arrayin icindeki tek ededlerin cemi:" + SumOfOdds);
+
+
+//int eded = 7;
+//string SingleAndDouble = math.SingleAndDouble(eded);
+//Console.WriteLine($"secilen eded " + SingleAndDouble);
+
+
+//int[] array = { 1, 2, 3, 4, 5 };
+//int SquareOfOdds = math.SquareOfOdds(array);
+//Console.WriteLine($"arrayin cut ededlerin kvadrati: "+SquareOfOdds);
+
+
+//int n = 5;
+//int SumOfNumb = math.SumOfNumb(n);
+//Console.WriteLine($"1den {n}-e qeder onaln ededlerin cemi: "+SumOfNumb);
+
+CustomMath math = new CustomMath();
+
+int[] nums = { 1, 2, 3, 4, 5, 6 };
+int SumOfOdds = math.SumOfOdds(nums);
+Console.WriteLine($"arrayin icindeki tek ededlerin cemi:" + SumOfOdds);
+
+int n = 9;
+string SingleAndDouble = math.SingleAndDouble(n);
+Console.WriteLine($"secilen eded " + SingleAndDouble);
+
+int[] array = { 1, 2, 3, 4, 5, 6 };
+int SquareOfOdds = math.SquareOfOdds(array);
+Console.WriteLine($"arrayin cut ededlerin kvadrati: " + SquareOfOdds);
+
+int x = 6;
+int SumOfNumbs = math.SumOfnumb(x);
+Console.WriteLine($"1den {n}-e qeder onaln ededlerin cemi: " + SumOfNumbs);
