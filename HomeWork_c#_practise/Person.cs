a082bada793e713754e27127450b3d3bba81fb9e
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-26 08:50:13 +0400

    Add files via upload

---
+++ HomeWork_c#_practise/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_c__practise
+{
+    internal class Person
+    {
+        public int id;
+        public string fullName;
+        public int age;
+        public string position;
+
+        
+
+        //public Person(int id, string fullName, int age, string position)
+        //{
+        //    this.id = id;
+        //    this.fullName = fullName;
+        //    this.age = age;
+        //    this.position = position;
+
+        //    //Console.WriteLine("hello sdsdd");
+
+
+        //}
+        //public Person():this("sad")
+        //{
+        //    Console.WriteLine("hello sdsdd");
+        //}
+        //public Person(string data):this(546)
+        //{
+        //    Console.WriteLine(data);
+        //}
+        //public Person(int num)
+        //{
+        //    Console.WriteLine("num");
+        //}
+
+        //public Person(string text, string s):this(text)
+        //{
+        //    Console.WriteLine("sadasd asd");
+        //}
+    }
+}   
+
+
+
+
+
+
+
+
+
---
+++ HomeWork_c#_practise/Program.cs
@@ -0,0 +1,110 @@
+﻿using HomeWork_c__practise;
+using System.Xml.Serialization;
+
+//Person person = new()
+//{
+//    id = 1 ,
+//    fullName = "fsdjf sfjk",
+//    age = 22,
+//    position = "student"
+//};
+
+//Console.WriteLine(person.fullName);
+
+//Person person = new(1, "asdasd sad", 45, "student");
+
+//Console.WriteLine(person.id);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Person person = new Person("asdasd");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Person person = new()
+//{
+// id = 1,
+// fullName = "sdsada",
+// age = 23
+//};
+
+//Person person2 = new()
+//{
+// id = 2,
+// fullName="sfdsf",
+// age =45
+//};
+
+//Person person3 = new()
+//{
+// id = 3,
+// fullName = "sdsad",
+// age = 54
+//};
+
+//Person person4 = new()
+//{
+// id =4,
+// fullName ="ssad",
+// age=48
+//};
+
+//Person[] people = { person, person2, person3, person4 };
+//void FindPersonById(Person[] people, int array)
+//{
+//    Person person = person FirstOrDefault(mbox => mbox.id == id);
+//    return person;
+//}
+//Person result = FindPersonById;
+//    string response = $"id: {result.id},fullname: {result.fullName} age: {result.age}"
+
+//Person result = FindPersonById(people,100);
+//void GetAllStudents(Person[] datas)
+//{
+//    return datas;
+//}
+//var allPeople = GetAllStudents(people);
+
+//if(result !=null)
+//{
+//    string responce = $"id: {result.id},fullname: {result.fullName} age: {result.age}";
+//    Console.WriteLine(responde);
+
+//}
+//else
+//{
+//    Console.WriteLine("data not found");
+//}
+
+//var result1 = result != null ? result : "sont found";
+//Console.WriteLine(result1);
+
+//void ShowAllpeople(Person[] datas)
+//{
+//    foreach (var item in datas)
+//    {
+//        string responce = $"id: {result.id},fullname: {result.fullName} age: {result.age}";
+//        Console.WriteLine(responde);
+
+//    }
+//}
+//ShowAllpeople(people);
+
+//ShowPerson(FindPersonById(GetAllStudents(), 1));
+
+
+
+//Person[] GetPEopleByName(Person[] people, string name)
+//{
+//    return people.Where(m => m.fullName == name).ToArray();
+//}
+//void ShowPeopleByName(Person[] datas)
+//{
+//    foreach (var item in datas)
+//    {
+//        string responce = $"id: {result.id},fullname: {result.fullName} age: {result.age}";
+//        Console.WriteLine(responde);
+
+//    }
+//}
+//ShowPeopleByName(GetPEopleByName(GetAllPErson(), "dsaasd"));
